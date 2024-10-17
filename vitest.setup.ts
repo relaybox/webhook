dcc551f779cbe97e5e10ae1cd3fbf0edd065805f
livecloud-labs <livecloud.store@gmail.com>
2024-10-17 12:20:44 +0100

    chore: add redis containe to pipeline

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,4 @@
-process.env.REDIS_HOST = 'localhost';
+process.env.REDIS_HOST = '127.0.0.1';
 process.env.REDIS_PORT = '6390';
 process.env.REDIS_TLS_DISABLED = 'true';
 process.env.LOG_LEVEL = 'debug';
