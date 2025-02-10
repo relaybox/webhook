ad55be4537a449ea967e769917d6ce796c9f2dbd
livecloud-labs <livecloud.store@gmail.com>
2025-02-10 10:01:10 +0000

    chore: add handlers test generated by rg

--- test/module/handlers.spec.ts
+++ test/module/handlers.spec.ts
@@ -1,7 +1,103 @@
-import { describe, expect, vi, it, beforeEach, afterEach } from 'vitest';
+// test/module/service.spec.ts
 
-describe('service', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+import { describe, it, expect, beforeAll, afterEach, afterAll, vi } from 'vitest';
+import { HttpResponse, http } from 'msw';
+import { setupServer } from 'msw/node';
+import { dispatchWebhook } from '@/module/service';
+import {
+  getMockWebhook,
+  getMockWebhookPayload,
+  mockWebhookEndpoint
+} from 'test/__mocks__/internal/webhook.mock';
+import { getLogger } from '@/util/logger.util';
+
+const logger = getLogger('webhook-dispatch');
+const server = setupServer();
+
+describe('dispatchWebhook', () => {
+  beforeAll(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+    vi.restoreAllMocks();
+  });
+
+  it('should dispatch a webhook successfully', async () => {
+    server.use(
+      http.post(mockWebhookEndpoint, () =>
+        HttpResponse.json({
+          status: 200
+        })
+      )
+    );
+
+    const webhook = getMockWebhook();
+    const payload = getMockWebhookPayload();
+    const response = await dispatchWebhook(logger, webhook, payload);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        id: payload.id,
+        status: 200,
+        timestamp: expect.any(Number)
+      })
+    );
+  });
+
+  it('should return standard webhook response from non-retryable error', async () => {
+    server.use(
+      http.post(mockWebhookEndpoint, () => {
+        return new HttpResponse(null, {
+          status: 404
+        });
+      })
+    );
+
+    const webhook = getMockWebhook();
+    const payload = getMockWebhookPayload();
+    const response = await dispatchWebhook(logger, webhook, payload);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        id: payload.id,
+        status: 404,
+        statusText: 'Not Found',
+        timestamp: expect.any(Number)
+      })
+    );
+  });
+
+  it('should throw an error if retryable error occurs (causing retry)', async () => {
+    server.use(
+      http.post(mockWebhookEndpoint, () => {
+        return new HttpResponse(null, {
+          status: 500
+        });
+      })
+    );
+
+    const webhook = getMockWebhook();
+    const payload = getMockWebhookPayload();
+
+    await expect(() => dispatchWebhook(logger, webhook, payload)).rejects.toThrow(
+      /Retryable HTTP error/
+    );
+  });
+
+  it('should throw an error if network error occurs', async () => {
+    server.use(
+      http.post(mockWebhookEndpoint, () => {
+        return HttpResponse.error();
+      })
+    );
+
+    const webhook = getMockWebhook();
+    const payload = getMockWebhookPayload();
+
+    await expect(() => dispatchWebhook(logger, webhook, payload)).rejects.toThrow(
+      /Failed to fetch/
+    );
   });
 });
